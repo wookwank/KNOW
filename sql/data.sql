fc41ba9970410d3d990b5464a149cf667b454117
Wookwan Kwon <wookwan@umich.edu>
2023-09-11 23:21:26 -0400

    setup complete

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO users(username, fullname)
+VALUES ('awdeorio', 'Andrew DeOrio');
---
+++ sql/schema.sql
@@ -0,0 +1,7 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users(
+  username VARCHAR(20) NOT NULL,
+  fullname VARCHAR(40) NOT NULL,
+  PRIMARY KEY(username)
+);
