2be92d996d9d5ce0a65201bc234fd3c276297631
Wookwan Kwon <wookwan@umich.edu>
2023-09-11 22:03:46 -0400

    Reset

--- insta485/views/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Views, one for each Insta485 page."""
-from insta485.views.index import show_index
--- sql/schema.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-CREATE TABLE users(
-  username VARCHAR(20) NOT NULL,
-  fullname VARCHAR(40) NOT NULL,
-  PRIMARY KEY(username)
-);
